3bbf7ed3ebf65f3f4d16ac3366550170064aa38b
jssilva93 <jssilvaf@poligran.edu.co>
2019-10-03 19:08:04 -0500

    picap maps

--- lib/picap_maps.rb
+++ lib/picap_maps.rb
@@ -1,6 +1,22 @@
 require "picap_maps/version"
+require "picap_maps"
 
 module PicapMaps
   class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :database_name
+
+    def initialize
+      @database_name = 'default'
+    end
+  end
 end
